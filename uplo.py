f2e3c000b9b6f2fdb67ce4cba3e1d0abbd7b5247
9842911312 <36265861+9842911312@users.noreply.github.com>
2018-03-13 16:25:44 +0530

    uplo.py
    Given a string change upper to lower case and lower to upper case

---
+++ uplo.py
@@ -0,0 +1,8 @@
+while True:
+	print("Enter 'x' for exit.")
+	string = input("Enter any string: ")
+	if string == 'x':
+		break
+	else:
+		string_in_uppercase = string.upper()
+		print("String in Uppercase =",string_in_uppercase,"\n")
