38190dca548c05669a29fafd803125b1c6c76a54
9842911312 <36265861+9842911312@users.noreply.github.com>
2018-03-13 15:51:22 +0530

    num3.py
    Given a number N and K followed by N numbers. Now given K numbers Ki means insert the number ki in the array and now print the maximum

---
+++ num3.py
@@ -0,0 +1,30 @@
+def find3numbers(arr):
+    n = len(arr)
+    max = n-1 
+    min = 0
+    smaller = [0]*10000
+    smaller[0] = -1
+    for i in range(1,n):
+        if (arr[i] <= arr[min]):
+            min = i
+            smaller[i] = -1
+        else:
+            smaller[i] = min
+    greater = [0]*10000
+    greater[n-1] = -1
+ 
+    for i in range(n-2,-1,-1):
+        if (arr[i] >= arr[max]):
+            max = i
+            greater[i] = -1
+        else:
+            greater[i] = max
+   
+    for i in range(0,n):
+        if smaller[i] != -1 and greater[i] != -1:
+            print arr[smaller[i]], arr[i], arr[greater[i]]
+            return
+    print "No triplet found"
+    return
+arr = [12, 11, 10, 5, 6, 2, 30]
+find3numbers(arr)
